c4ca752dfac90e9ca223ca7259351ea4af1a5ab6
aoashwin <aoashwin29.aa@gmail.com>
2024-03-13 17:57:16 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <p>HOME</p>
+      
+    </div>
+
+  )
+}
+
+export default page
